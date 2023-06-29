14752833e50856914ccdab90466aecb5cb6e0bcc
Roger Retita <roger.retita@gmail.com>
2023-06-29 17:27:14 +0200

    premier filtre

--- src/components/Table/dataTable.js
+++ src/components/Table/dataTable.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Styles from './styles.module.css';
 
 const TabList = (props) => {
+  const [sortOrder, setSortOrder] = useState('asc');
+  const [sortedColumn, setSortedColumn] = useState('firstName');
+
+  const handleSort = (columnName) => {
+    if (columnName === sortedColumn) {
+      setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    } else {
+      setSortOrder('asc');
+      setSortedColumn(columnName);
+    }
+  };
+
+  const getSortIcon = (columnName) => {
+    if (columnName === sortedColumn) {
+      if (sortOrder === 'asc') {
+        return <span>&uarr;</span>; // Icône direction haut
+      } else {
+        return <span>&darr;</span>; // Icône direction bas
+      }
+    } else {
+      return null;
+    }
+  };
+
+  const sortedEmployees = [...props.employees].sort((a, b) => {
+    const valueA = a[sortedColumn];
+    const valueB = b[sortedColumn];
+    const comparison = valueA.localeCompare(valueB);
+
+    return sortOrder === 'asc' ? comparison : -comparison;
+  });
+
   return (
     <div className={Styles["tablist-container"]}>
       <input
@@ -13,19 +45,25 @@ const TabList = (props) => {
       <table className={Styles["tablist-table"]}>
         <thead>
           <tr>
-            <th>First Name</th>
-            <th>Last Name</th>
+            <th onClick={() => handleSort('firstName')}>
+              First Name {getSortIcon('firstName')}
+            </th>
+            <th onClick={() => handleSort('lastName')}>
+              Last Name {getSortIcon('lastName')}
+            </th>
             <th>Birth</th>
             <th>Start Date</th>
             <th>Street</th>
             <th>City</th>
             <th>State</th>
-            <th>Zip Code</th>
+            <th onClick={() => handleSort('zipCode')}>
+              Zip Code {getSortIcon('zipCode')}
+            </th>
             <th>Département</th>
           </tr>
         </thead>
         <tbody>
-          {props.employees.map((employee, index) => (
+          {sortedEmployees.map((employee, index) => (
             <tr key={index}>
               <td>{employee.firstName}</td>
               <td>{employee.lastName}</td>
